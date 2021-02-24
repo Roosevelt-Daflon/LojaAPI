079a559d3b9b1510c25f0ac62dedf543b42a4806
Roosevelt Daflon <rooseveltdaflon@hotmail.com>
2021-02-24 14:55:37 -0300

    att 2

---
+++ Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LojaAPI.Models;
+
+namespace LojaAPI.Data
+{
+	public class DataContext : DbContext
+	{
+
+		public DataContext(DbContextOptions<DataContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<Category> Categories { get; set; }
+	}
+}
--- Models/Category.cs
+++ Models/Category.cs
@@ -5,11 +5,11 @@ namespace LojaAPI.Models
     public class Category
     {
         [Key]
-        public int id{get; set;}
+        public int Id{get; set;}
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
 		[MaxLength(60, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
 		[MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
-		public string title {get; set;}
+		public string Title {get; set;}
     }
 }
